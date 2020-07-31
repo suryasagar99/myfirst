5a76a45d96211d347d4046f8702a3d39678597ac
suryasagar99 <gsuryasagar@gmail.com>
2020-07-31 19:48:05 +0530

    sol3.0

---
+++ site/js/script.js
@@ -0,0 +1,13 @@
+
+
+(function(window){var s1={};
+
+var name="sagar";
+s1.fun1=function(){
+
+	console.log(name);
+
+};
+window.s1=s1;
+
+})(window);
---
+++ site/js/script1.js
@@ -0,0 +1,12 @@
+(
+function(window){
+var s2={};
+var name="surya";
+s2.fun2=function(){
+
+	console.log(name);
+}
+window.s2=s2;
+}
+
+)(window);
